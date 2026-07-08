f3495e8c82500f07f827802d76834a912a63061e
Jan Johannes <jan@ntr.io>
2026-07-08 12:56:31 +0300

    update to darc 0.0.11

--- macos/Package.swift
+++ macos/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "macos",
     platforms: [
-        .macOS(.v26)
+        .macOS("26.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
